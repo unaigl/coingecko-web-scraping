936a5c292b826bcb77b6bfead794d1e758bfec7a
unaigl <unaiarki@gmail.com>
2022-07-13 17:40:09 +0200

    fixing async issue

--- scraping.js
+++ scraping.js
@@ -7,22 +7,22 @@ const puppeteer = require("puppeteer");
   await page.goto("https://www.coingecko.com/es");
 
   // Evaluating page's DOM elements and extracting data
-  const { urls, tickers } = await page.evaluate(() => {
+  const { urls, rawTickers } = await page.evaluate(() => {
     // Extracting DOM elemets to an array // Contains href and ticker
-    const rawUrls = document.querySelectorAll(".coin-name a");
+    const _rawUrls = document.querySelectorAll(".coin-name a");
 
     // Looping to extract and filter data
-    const urls = [];
-    const tickers = [];
-    for (let i = 0; i < rawUrls.length; i++) {
-      let _url = rawUrls[i].href;
-      urls.push(_url);
+    const _urls = [];
+    const _tickers = [];
+    for (let i = 0; i < _rawUrls.length; i++) {
+      let _url = _rawUrls[i].href;
+      _urls.push(_url);
 
       // Taking the "ticker" of each token/coin
-      tickers.push(rawUrls[i].firstChild.nodeValue);
+      _tickers.push(_rawUrls[i].firstChild.nodeValue);
     }
 
-    return { urls, tickers };
+    return { _urls, _tickers };
   });
 
   // Filtering and cleaning data
@@ -32,30 +32,36 @@ const puppeteer = require("puppeteer");
   let finalTickers = [];
   let tokenNames = [];
   // Extracting only the tickers we've inside our array
-  for (let i = 0; i < tickers.length; i++) {
-    let str = tickers[i].trim();
+  // @dev - Be aware of rawTickers value, because is defined by an async method
+  for (let i = 0; i < rawTickers.length; i++) {
+    let str = rawTickers[i].trim();
     // console.log(str);
     if (str === str.toUpperCase()) {
-      finalTickers.push(tickers[i]);
+      finalTickers.push(rawTickers[i]);
     } else {
-      tokenNames.push(tickers[i]);
+      tokenNames.push(rawTickers[i]);
     }
   }
-  console.log("aa", finalTickers);
-  console.log("ccc", tokenNames);
+
+  // console.log("aa", finalTickers);
+  // console.log("ccc", tokenNames);
+  // console.log("ccc", setUrls);
 
   // Obteniendo el contrato
-  // for (let i = 0; i < /* webUrls.length */ 3; i++) {
-  //   await page.goto(webUrls[i]);
+  for (let i = 0; i < 3; i++) {
+    await page.goto(webUrls[i]);
 
-  //   try {
-  //     await page.get;
-  //   } catch (error) {
-  //     console.log("Has not an address, is a Coin");
-  //   }
+    try {
+      const _rawElemet = document.querySelectorAll(
+        `[data-symbol="${finalTickers[i]}"]`
+      );
+      console.log(_rawElemet);
+    } catch (error) {
+      console.log("Has not an address, is a Coin");
+    }
 
-  //   await page.goBack();
-  // }
+    await page.goBack();
+  }
 
   await browser.close();
 })();
