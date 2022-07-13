1dd511b606259dd04ec363ee0eace841a034a287
unaigl <unaiarki@gmail.com>
2022-07-13 16:26:34 +0200

    filtering data

--- scraping.js
+++ scraping.js
@@ -1,12 +1,12 @@
 const puppeteer = require("puppeteer");
 
 (async () => {
-  // Opening a browser at coingecko page
+  // Opening a browser at coingecko website
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.goto("https://www.coingecko.com/es");
 
-  // Evaluating page DOM elements and extracting data
+  // Evaluating page's DOM elements and extracting data
   const { urls, tickers } = await page.evaluate(() => {
     // Extracting DOM elemets to an array // Contains href and ticker
     const rawUrls = document.querySelectorAll(".coin-name a");
@@ -18,39 +18,39 @@ const puppeteer = require("puppeteer");
       let _url = rawUrls[i].href;
       urls.push(_url);
 
-      // Taking the "ticker and name" of each token/coin
+      // Taking the "ticker" of each token/coin
       tickers.push(rawUrls[i].firstChild.nodeValue);
     }
-    // Deleting duplicated data
-    const setUrls = [...new Set(urls)];
 
-    const finalTickers = tickers.filter((ticker) => {
-      let _finalTickers = [];
-
-      let _splitted = ticker.split("");
-      let _counter = 0;
-      let _tick = "";
-      for (let i = 0; i < _splitted.length; i++) {
-        if (_splitted[i] === _splitted[i].toUpperCase()) {
-          _counter++;
-          _tick += _splitted[i];
-        }
-      }
-      if (_counter >= 2) {
-        _finalTickers.push(_tick);
-        return _finalTickers;
-      }
-      return false;
-    });
-
-    return { setUrls, finalTickers };
+    return { urls, tickers };
   });
 
-  // console.log(webUrls.length);
-  // console.log(webUrls);
-  // console.log(names);
-  console.log(tickers);
+  // Deleting duplicated data
+  const setUrls = [...new Set(urls)];
+  // Cleaning our tickers data array
+  const finalTickers = tickers.filter((ticker) => {
+    let _finalTickers = [];
+    // Extracting only the tickers we've inside our array
+    let _splitted = ticker.split("");
+    var _counter = 0;
+    let _tick = "";
+    for (let i = 0; i < _splitted.length; i++) {
+      if (_splitted[i] === _splitted[i].toUpperCase()) {
+        _counter++;
+        _tick += _splitted[i];
+      }
+    }
+    if (_counter >= 2) {
+      _finalTickers.push(_tick);
+      return _finalTickers;
+    }
+    return false;
+  });
 
+  console.log(setUrls);
+  console.log(finalTickers);
+
+  // Obteniendo el contrato
   // for (let i = 0; i < /* webUrls.length */ 3; i++) {
   //   await page.goto(webUrls[i]);
 
